d82b3ae1dd2eb7aa82bd276c87617e45ed4e6b1d
Guilaume Wafo-Tapa <gwafotapa@gmail.com>
2023-05-28 10:47:35 +0200

    Setup googletest

---
+++ test/add_authors_test.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+
+TEST(AddAuthors, EmptyName) {}
+
+TEST(AddAuthors, BlankName) {}
+
+TEST(AddAuthors, DuplicateName) {}
+
+TEST(AddAuthors, TwoNames) {}
+
+TEST(AddAuthors, ThreeNames) {}
+
+TEST(AddAuthors, TwoIdenticalNames) {}
